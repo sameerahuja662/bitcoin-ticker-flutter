357bfae13d7f76a3b80be3058a6ea94f83476e7f
Sameer Ahuja <sameerahuja662@gmail.com>
2020-08-09 17:32:14 -0400

    Added CoinAPI functionality

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,5 @@
+import 'networking.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +30,29 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apiKey = '3D47FECD-6CF5-4907-86DB-78F07A6ADF1B';
+const coinURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  var cryptoFiatValues = {'BTC': 0.0, 'ETH': 0.0, 'LTC': 0.0};
+
+  Future<Map<String, double>> getCryptoRates(
+      String selectedFiatCurrency) async {
+    for (String cryptoCurrency in cryptoList) {
+      var requestURL =
+          '$coinURL/$cryptoCurrency/$selectedFiatCurrency?apikey=$apiKey';
+      NetworkHelper networkHelper = NetworkHelper(requestURL);
+
+      var coinData = await networkHelper.getData();
+      print(coinData);
+      if (coinData != null) {
+        double result = coinData['rate'];
+        cryptoFiatValues[cryptoCurrency] =
+            double.parse(result.toStringAsFixed(3));
+      } else {
+        return null;
+      }
+    }
+    return cryptoFiatValues;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +9,32 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = currenciesList[0];
+  double btcFiatRate = 0;
+  double ethFiatRate = 0;
+  double ltcFiatRate = 0;
+  CoinData coinData = CoinData();
+
+  void getCoinData() async {
+    Map<String, double> fiatRates =
+        await coinData.getCryptoRates(selectedCurrency);
+    if (fiatRates != null) {
+      setState(() {
+        btcFiatRate = fiatRates['BTC'];
+        ethFiatRate = fiatRates['ETH'];
+        ltcFiatRate = fiatRates['LTC'];
+      });
+    } else {
+      print('Print Network error');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCoinData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +56,49 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $btcFiatRate $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ETH = $ethFiatRate $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 LTC = $ltcFiatRate $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,10 +113,134 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            //Get Platform specific menu
+            child: Platform.isIOS ? getIosPicker() : getAndroidPicker(),
           ),
         ],
       ),
     );
   }
+
+  DropdownButton getAndroidPicker() {
+    //Prepare List of currencies for DropDownMenu.
+    List<DropdownMenuItem<String>> dropDownMenuItems = [];
+    for (String currency in currenciesList) {
+      //Initialise new dropDownMenuItem items.
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      //Add menu items to the list.
+      dropDownMenuItems.add(newItem);
+    }
+
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropDownMenuItems,
+      onChanged: (value) async {
+        setState(() {
+          selectedCurrency = value;
+        });
+        Map<String, double> fiatRates =
+            await coinData.getCryptoRates(selectedCurrency);
+        setState(() {
+          btcFiatRate = fiatRates['BTC'];
+          ethFiatRate = fiatRates['ETH'];
+          ltcFiatRate = fiatRates['LTC'];
+        });
+      },
+    );
+  }
+
+  CupertinoPicker getIosPicker() {
+    List<Text> currencyPickerList = [];
+
+    for (String currency in currenciesList) {
+      currencyPickerList.add(Text(currency));
+    }
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) async {
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+        });
+        Map<String, double> fiatRates =
+            await coinData.getCryptoRates(selectedCurrency);
+        setState(() {
+          btcFiatRate = fiatRates['BTC'];
+          ethFiatRate = fiatRates['ETH'];
+          ltcFiatRate = fiatRates['LTC'];
+        });
+      },
+      children: currencyPickerList,
+    );
+  }
 }
+
+/*
+
+items: [
+DropdownMenuItem(
+child: Text('USD'),
+value: 'USD',
+),
+DropdownMenuItem(
+child: Text('EUR'),
+value: 'EUR',
+),
+DropdownMenuItem(
+child: Text('GBP'),
+value: 'GBP',
+),
+DropdownMenuItem(
+child: Text('INR'),
+value: 'INR',
+),
+],*/
+
+/*
+
+currenciesList.map<DropdownMenuItem<String>>((String value) {
+return DropdownMenuItem<String>(
+value: value,
+child: Text(value),
+);
+}).toList(),*/
+
+/*
+DropdownButton<String>(
+value: selectedCurrency,
+items:
+,
+onChanged: (value) {
+setState(() {
+selectedCurrency = value;
+});
+},
+)*/
+
+/*//Prepare List of currencies for DropDownMenu.
+  List<DropdownMenuItem> getCurrencyItems() {
+    List<DropdownMenuItem<String>> dropDownMenuItems = [];
+    for (String currency in currenciesList) {
+      //Initialise new dropDownMenuItem items.
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      //Add menu items to the list.
+      dropDownMenuItems.add(newItem);
+    }
+
+    return dropDownMenuItems;
+  }*/
+
+/*List<Text> getPickerItems() {
+    List<Text> currencyPickerList = [];
+
+    for (String currency in currenciesList) {
+      currencyPickerList.add(Text(currency));
+    }
+    return currencyPickerList;
+  }*/
